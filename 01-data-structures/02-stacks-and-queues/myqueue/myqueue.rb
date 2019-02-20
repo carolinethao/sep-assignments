5b6fd267c954f54555d01a357151358e9374ad61
Caroline Nguyen <caroline.nguyen@outlook.com>
2019-02-20 14:38:46 -0600

    c02 - completed

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,22 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue.last
   end
 
   def enqueue(element)
+    @queue << element
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def dequeue
+    your_turn = @queue.shift
+    @head = @queue[0]
+    @tail = @queue.last
   end
 
   def empty?
+    @queue.empty?
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,21 @@ class MyStack
   end
 
   def push(item)
+    @stack << item
+    @top = @stack.last
   end
 
   def pop
+    item = @top
+    if !empty?
+      # Last In First Out
+      @stack.delete_at(@stack.length - 1)
+      @top = @stack.last
+    end
+    item
   end
 
   def empty?
+    @stack.empty?
   end
 end
