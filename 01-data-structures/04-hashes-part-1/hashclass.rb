68f45f766e8e0c5e5832c854a417bc2c523db1a7
Caroline Nguyen <caroline.nguyen@outlook.com>
2019-03-13 17:54:39 -0500

    c04 - completed

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -5,23 +5,42 @@ class HashClass
   end
 
   def []=(key, value)
+    item = HashItem.new(key, value)
+
+    i = index(key, size)
+
+    if @items[i].nil?
+      @items[i] = item
+    elsif @items[i].key != item.key
+      self.resize
+      self[key] = value
+    elsif @items[i].value != item.value
+      self.resize
+      @items[index(item.key, size)] = value
+    end
   end
 
 
   def [](key)
+    @items[self.index(key, self.size)].value
   end
 
   def resize
+    hold_items = @items.compact
+    @items = Array.new(self.size * 2)
+    hold_items.each { |item| self[item.key] = item.value }
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.count
   end
 
 end
